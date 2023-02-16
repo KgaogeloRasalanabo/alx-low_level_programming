9c7ba9cd53e46bd9046e71c2e5d2a5f58870f8e0
Kgaogelo Ishmael Rasalanabo <ishael.rasal@gmail.com>
2023-02-16 03:33:49 -0800

    C print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
-/* print in C */
+/**
+ * print in C
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
