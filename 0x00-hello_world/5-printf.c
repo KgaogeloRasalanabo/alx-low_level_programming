795835a46d37225c63d0d53b12f756cff78e6668
Kgaogelo Ishmael Rasalanabo <ishael.rasal@gmail.com>
2023-02-16 03:56:00 -0800

    print sentence

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
  * main - print exactly "with proper grammar,
  * - but the outcome is a piece of art,"
- * Return: 0 
+ * Return: 0 if exited properly, non-zero otherwise 
  */
 int main(void)
 {
