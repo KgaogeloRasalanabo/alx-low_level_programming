832eaf64597253eabb35cc2a1d628a1d368be479
Kgaogelo Ishmael Rasalanabo <ishael.rasal@gmail.com>
2023-02-16 04:08:57 -0800

    print sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - print exactly "Programming is like building a multilingual puzzle"
+ * main - Entry point
  *
- * return: 0
+ * Return: always 0
  */
 int main(void)
 {
