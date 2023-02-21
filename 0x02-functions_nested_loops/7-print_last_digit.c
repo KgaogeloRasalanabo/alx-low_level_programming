f9fe2b4b67c8194e50e3dba11814ba4e8f99ab61
Kgaogelo Ishmael Rasalanabo <ishael.rasal@gmail.com>
2023-02-21 07:31:42 -0800

    last digit on num

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,12 +4,12 @@
  * print_last_digit - main function
  *
  * @n: integer to get last digit of
- * Return: last digit of i
+ * Return: last digit of n
  *
  */
 
-int print_last_digit(int i)
+int print_last_digit(int n)
 {
-	_putchar('0' + _abs(i % 10));
-	return (_abs(i % 10));
+	_putchar('0' + _abs(n % 10));
+	return (_abs(n % 10));
 }
