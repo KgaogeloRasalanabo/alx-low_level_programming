8731d08ec9ebd98854e7b10adb3472a9fe8987e2
Kgaogelo Ishmael Rasalanabo <ishael.rasal@gmail.com>
2023-02-21 00:08:58 -0800

    print using Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main -  prints -Putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('Putchar')
+	_putchar('\n');
+
+	return (0);
+}
